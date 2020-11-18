790ce261e38e460009b03d75f4ca0fc955bf4189
masafumi kato <xslash1105@gmail.com>
2020-11-18 09:18:05 +0900

    [add] prodcut page

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,24 +1,30 @@
 import React from 'react'
 import "./Product.css";
 
-function Product() {
+interface Props {
+    title: string;
+    price: number;
+    rate: number;
+    image: string;
+}
+
+function Product(props: Props) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The Lean startup</p>
+                <p>{props.title}</p>
                 <p className="product__price">
-                    <strong>1,500</strong>
+                    <strong>{props.price}</strong>
                     <small>円</small>
                 </p>
                 <div className="product__raiting">
-                    <p>⭐️</p>
-                    <p>⭐️</p>
-                    <p>⭐️</p>
-                    <p>⭐️</p>
+                    {Array(props.rate).fill('⭐️').map((v, i) => {
+                        return <p>{v}</p>
+                    })}
                 </div>
             </div>
 
-            <img src="https://m.media-amazon.com/images/I/41vP838nOUL._AC_SL520_.jpg" alt=""/>
+            <img src={props.image} alt={props.title} />
 
             <button>Add to Basket</button>
         </div>
