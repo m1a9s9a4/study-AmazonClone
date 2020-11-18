053e3acd4ed958a65b352548682ffcb4e1c267f1
masafumi kato <xslash1105@gmail.com>
2020-11-18 09:18:23 +0900

    [add] home

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,13 +9,32 @@ function Home() {
                 <img src="https://images-fe.ssl-images-amazon.com/images/G/09/kindle/journeys/MGVjYjdhYTct/MGVjYjdhYTct-OGVjNTJmZjgt-w3000._CB417511840_.jpg" alt="" className="home__image"/>
 
                 <div className="home__row">
-                    <Product />
-                    <Product />
+                    <Product 
+                        title="test1"
+                        price={1500}
+                        rate={5}
+                        image="https://m.media-amazon.com/images/I/41vP838nOUL._AC_SL520_.jpg" 
+                    />
+                    <Product 
+                        title="test2"
+                        price={1000}
+                        rate={2}
+                        image="https://m.media-amazon.com/images/I/41vP838nOUL._AC_SL520_.jpg" 
+                    />                    
                 </div>
                 <div className="home__row">
-                    {/* product */}
-                    {/* product */}
-                    {/* product */}
+                    <Product 
+                        title="test3"
+                        price={2500}
+                        rate={4}
+                        image="https://m.media-amazon.com/images/I/41vP838nOUL._AC_SL520_.jpg" 
+                    />
+                    <Product 
+                        title="tes3"
+                        price={2500}
+                        rate={4}
+                        image="https://m.media-amazon.com/images/I/41vP838nOUL._AC_SL520_.jpg" 
+                    />
                 </div>
                 <div className="home__row">
                     {/* product */}
